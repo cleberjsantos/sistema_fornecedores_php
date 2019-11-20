0af3d1c350c590f822bd0d2b81c89f6ee7505b4d
Cleber J Santos <cleber@cleberjsantos.com.br>
2019-11-20 14:52:54 -0300

    Fix Queries

--- queries.sql
+++ queries.sql
@@ -1,36 +1,37 @@
-CREATE DATABASE '7thorsystems' --*** 
+CREATE DATABASE IF NOT EXISTS 7thorsystems;
+GRANT ALL ON 7thorsystems.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
+
 -- ***trocar pelo nome do banco, não serão criados outros usuários além do root, como o banco funciona em uma estrutura pequena não usaremos indices
 
 CREATE TABLE IF NOT EXISTS fornecedores(
-    id_forn INT (11),
-    nome_empresa VARCHAR (80),
-    nome_fantasia VARCHAR (80),
-    endereco VARCHAR (50),
-    cep INT(20),
-    cnpj INT(20),
-    contato INT (20),
-    email VARCHAR (30),
-    constraint fornecedores_pk PRIMARY KEY AUTO_INCREMENT(id_forn)
-);
+   id_forn INT PRIMARY KEY AUTO_INCREMENT,
+   nome_empresa VARCHAR (160),
+   nome_fantasia VARCHAR (160),
+   endereco VARCHAR (130),
+   cep INT(20),
+   cnpj VARCHAR(20),
+   contato VARCHAR (20),
+   email VARCHAR (60)
+) ENGINE = MyISAM;
 
 CREATE TABLE IF NOT EXISTS produtos(
-    id_produto INT (11),
-    id_forn INT (10),
-    nome_produto VARCHAR (80),
-    modelo VARCHAR (50),
-    lote INT (20),
-    preco FLOAT (5, 2),
-    quantidade INT (20),
-    constraint produto_pk PRIMARY KEY AUTO_INCREMENT (id_produto),
-    constraint fornecedores_produtos_fk FOREIGN KEY (id_forn)
-    REFERENCES fornecedores (id_forn)
-);
+   id_produto INT (11),
+   id_forn INT (10),
+   nome_produto VARCHAR (160),
+   modelo VARCHAR (80),
+   lote INT (20),
+   preco FLOAT (5, 2),
+   quantidade INT (20),
+   constraint produto_pk PRIMARY KEY AUTO_INCREMENT (id_produto),
+   constraint fornecedores_produtos_fk FOREIGN KEY (id_forn)
+   REFERENCES fornecedores (id_forn)
+) ENGINE = MyISAM;
 
 CREATE TABLE IF NOT EXISTS usuarios (
-    id_usuario INT (11),
-    login VARCHAR(20) NULL,
-    senha VARCHAR(20) NULL,
-    constraint id_usuario_pk PRIMARY KEY AUTO_INCREMENT(id_user)
-)
+           id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+           login VARCHAR(30),
+           senha VARCHAR(60)
+) ENGINE = MyISAM;
 
-INSERT INTO usuarios (login, senha) VALUES ('root','123mudar4');
+INSERT INTO usuarios(login, senha) VALUES ('root', '$senha');
